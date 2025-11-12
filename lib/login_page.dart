ba1fbe94424bae75292a906152a619c41359ef5f
WoutersPlayground <mayandha123@gmail.com>
2025-11-12 12:34:54 +0700

    perbaiki

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -208,7 +208,6 @@ class _LoginPageState extends State<LoginPage> {
                     ),
 
                     const SizedBox(height: 8),
-
                     // button
                     SizedBox(
                       width: double.infinity,
